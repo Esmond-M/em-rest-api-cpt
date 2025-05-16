a3900ac44a0aafbd235c5d0a20d18e0df50fb2f0
Esmond <esmondmccain@gmail.com>
2025-05-16 14:17:40 -0500

    Initial commit

---
+++ em-rest-api-cpt.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Plugin Name: EM Rest API CPT
+ * Plugin URI: https://github.com/Esmond-M
+ * Author: Esmond Mccain
+ * Author URI: https://esmondmccain.com/
+ * Description: Use REST API to populate custom post type. 
+ * Version: 0.1.0
+ * License: 0.1.0
+ * License URL: http://www.gnu.org/licenses/gpl-2.0.txt
+ * text-domain: em-rest-api-cpt
+*/
+namespace  em_rest_api_cpt\init_plugin;
+
+
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+final class em_rest_api_cpt {
+
+    const VERSION = '0.1.0';
+    const PHP_MINIMUM_VERSION = '7.0';
+
+    private static $_instance = null;
+
+    public function __construct() {
+        add_action( 'init', [ $this, 'i18n' ] );
+        add_action( 'plugins_loaded', [ $this, 'init_plugin' ] );
+
+    }
+
+    public function i18n() {
+        load_plugin_textdomain( 'em-rest-api-cpt' );
+    }
+
+    public function init_plugin() {
+        // Check php version
+    }
+
+    public static function get_instance() {
+
+        if ( null == self::$_instance ) {
+            self::$_instance = new Self();
+        }
+
+        return self::$_instance;
+
+    }
+
+
+}
+
+em_rest_api_cpt::get_instance();
---
+++ readme.md
@@ -0,0 +1,13 @@
+Project: [https://github.com/Esmond-M/em-rest-api-cpt](https://github.com/Esmond-M/em-rest-api-cpt)<br>
+Author: [esmondmccain.com](https://esmondmccain.com/)
+
+## Features
+Use REST API to populate custom post type. 
+ ## Installation
+
+1. Download the latest version from [https://github.com/Esmond-M/em-rest-api-cpt/blob/main/build/em-rest-api-cpt.zip](https://github.com/Esmond-M/em-rest-api-cpt/blob/main/build/em-rest-api-cpt.zip).
+2. Upload `em-rest-api-cpt` zip to the `/wp-content/plugins/` directory.
+3. Extract zip folder. Folder name of plugin should be "em-rest-api-cpt".
+4. Activate the plugin through the 'Plugins' menu in WordPress.
+
+
